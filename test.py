a2fa8b91de1098ec40a4348ebd784599574ac58e
Lucerodelaluz <itzeldenissetobias@gmail.com>
2025-10-14 18:17:49 -0600

    Prueba conexion20

--- test.py
+++ test.py
@@ -4,9 +4,13 @@ from calc import suma
 def test_suma_numeros():
  # Prueba 1: suma de dos numeros enteros
     assert suma(2, 3) == 5
+# Prueba 10: suma de dos numeros enteros
+    assert suma(3, 3) == 6
  # Prueba 2: suma de dos numeros decimales
     assert suma(2.5, 3.5) == 6.0
  # Prueba 3: suma de numero entero y numero decimal
     assert suma(2, 3.5) == 5.5
  # Prueba 4: manejo de error al recibir un texto
     assert suma(2, "texto") == "error"
+
+    
